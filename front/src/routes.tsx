ab8796ef5c125ba113db593a39474bd6555e870f
Antero Júnior <anterojunior1530@gmail.com>
2024-10-13 00:49:29 -0300

    feat: register component

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Login } from "./components/login/Login";
+import { Register } from "./components/register/Register";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Login />,
+    },
+    {
+        path: '/register',
+        element: <Register />
     },
 ]);
 
